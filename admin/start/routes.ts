e955dc192c6c9c7705a38064a849005d6dccf195
Henry Estela <hestela@mailbox.org>
2026-04-19 22:51:50 -0700

    feat(zim): add zim uploader in content manager
    adds a collapsible file uploader to accept zim file uploads into kiwix.

--- admin/start/routes.ts
+++ admin/start/routes.ts
@@ -205,6 +205,7 @@ router
     router.post('/download-remote', [ZimController, 'downloadRemote'])
     router.post('/download-category-tier', [ZimController, 'downloadCategoryTier'])
 
+    router.post('/upload', [ZimController, 'upload'])
     router.get('/wikipedia', [ZimController, 'getWikipediaState'])
     router.post('/wikipedia/select', [ZimController, 'selectWikipedia'])
 
